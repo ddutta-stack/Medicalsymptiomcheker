58384f7b648dd91e9117cef7e7eeec1fbe6b3800
ddutta <dibyendu.dutta08@gmail.com>
2025-06-30 23:11:08 +0530

    Added more code

--- symptom_check_UI.py
+++ symptom_check_UI.py
@@ -1,4 +1,6 @@
 #Symptom checker UI based implementation using Gradio.
+# This code provides a user interface for the symptom checker using Gradio.
+# It allows users to input their symptoms and receive a possible condition based on those symptoms.
 import gradio as gr
 import random
 ollamaurl = "http://localhost:11434/api/generate"
